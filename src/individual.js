92faed28afe1e6e4b289dbd4aeacf94dabdb2a26
Corey Ferguson <corey.t.ferguson@gmail.com>
2015-12-19 15:21:49 -0800

    Fitness function.

--- src/individual.js
+++ src/individual.js
@@ -4,10 +4,10 @@
  * @param {string} genotype
  */
 var Individual = function(genotype) {
-	if (genotype === undefined || genotype === null || genotype === '') {
-		throw new Error('Illegal argument: missing required `genotype`.');
-	}
-	this._genotype = genotype;
+  if (genotype === undefined || genotype === null || genotype === '') {
+    throw new Error('Illegal argument: missing required `genotype`.');
+  }
+  this._genotype = genotype;
 };
 
 /**
@@ -18,15 +18,34 @@ var Individual = function(genotype) {
  * @returns {(string|Individual)}
  */
 Individual.prototype.genotype = function(g) {
-	if (g === undefined) {
-		return this._genotype;
-	} else {
-		if (g === null || g === '') {
-			throw new Error('Illegal argument: genotype cannot be null.');
-		}
-		this._genotype = g;
-		return this;
-	}
+  if (g === undefined) {
+    return this._genotype;
+  } else {
+    if (g === null || g === '') {
+      throw new Error('Illegal argument: genotype cannot be null.');
+    }
+    this._genotype = g;
+    return this;
+  }
+};
+
+/**
+ * Calculate fitness score of this individual. Compare against the given
+ * target value.
+ * @param {string} target Value to compare genotype against.
+ * @returns {integer} Returns 1 point for every character in the same location as target.
+ */
+Individual.prototype.fitness = function(target) {
+  if (target === null || target === undefined) {
+    throw new Error('Illegal argument: target cannot be null or undefined');
+  }
+  var fitness = 0;
+  for (var c=0; c<target.length && c<this._genotype.length; c++) {
+    if (this._genotype.charAt(c) === target.charAt(c)) {
+      fitness++;
+    }
+  }
+  return fitness;
 };
 
 module.exports = Individual;
